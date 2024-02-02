74878280b6fc38e09659d154d9395a9a1e2c4ff5
Maria Baltar <merybaltar@gmail.com>
2024-02-02 17:12:49 -0300

    improves list mapping

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -7,8 +7,15 @@ const Recipe = () => {
   return (
     <div>
       <h2>{recipes[0].title}</h2>
-      <IngredientList ingredientList={recipes[0].preparations[0].ingredients} />
-      <StepList steps={recipes[0].preparations[0].steps} />
+      {recipes[0].preparations.map((preparation) => {
+        return (
+          <div>
+            {preparation.title && <h3>{preparation.title}</h3>}
+            <IngredientList ingredientList={preparation.ingredients} />
+            <StepList steps={preparation.steps} />
+          </div>
+        );
+      })}
       {recipes[0].source && <p>Fuente: {recipes[0].source?.author}</p>}
     </div>
   );
