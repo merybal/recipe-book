a1810e3490294bdc53a890bafe2ea2303be51c6a
Maria Baltar <merybaltar@gmail.com>
2025-05-27 15:50:42 -0300

    improves button component and adds border mixin

--- apps/frontend/src/design-system/Button.tsx
+++ apps/frontend/src/design-system/Button.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import Icon from "./Icon";
+
+import { IconName } from "./Icons";
 
 import clsx from "clsx";
 
@@ -7,20 +9,28 @@ import styles from "./Button.module.scss";
 type ButtonProps = {
   className?: string;
   disabled?: boolean;
-  iconLeft?: string;
-  iconRight?: string;
+  disruptive?: boolean;
+  iconLeft?: IconName;
+  iconRight?: IconName;
   inline?: boolean;
   label: string;
   size?: "small" | "medium" | "large";
   type?: "button" | "submit" | "reset";
-  variant?: "primary" | "secondary";
-  loading?: boolean;
+  variant?: "primary" | "secondary" | "tertiary";
+  // loading?: boolean;
   onClick: () => void;
 };
+
+const iconSizeMap = {
+  small: "xs",
+  medium: "sm",
+  large: "md",
+} as const;
 
 const Button = ({
   className,
   disabled = false,
+  disruptive = false,
   iconLeft,
   iconRight,
   inline,
@@ -28,20 +38,28 @@ const Button = ({
   size = "medium",
   type = "button",
   variant = "primary",
-  loading = false,
+  // loading = false,
   onClick,
 }: ButtonProps) => {
+  const pixelSize = iconSizeMap[size];
+
   return (
     <button
       className={clsx(
         styles.button,
+        { [styles[`variant-${variant}`]]: variant },
+        { [styles[`disruptive-variant-${variant}`]]: disruptive },
         { [styles[`size-${size}`]]: size },
+        { [styles["full-width"]]: !inline },
         className
       )}
       disabled={disabled}
+      type={type}
       onClick={onClick}
     >
+      {iconLeft && <Icon name={iconLeft} size={pixelSize} />}
       {label}
+      {iconRight && <Icon name={iconRight} size={pixelSize} />}
     </button>
   );
 };
--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import clsx from "clsx";
 import type { PreviewData } from "../components/TileGrid";
+import { Button } from "@/design-system";
 
 import styles from "./Home.module.scss";
 import TileGrid from "../components/TileGrid";
@@ -33,6 +34,52 @@ const Home = () => {
 
   return (
     <div className={clsx(styles.home)}>
+      <Button
+        label="Prueba"
+        iconLeft="wheat"
+        iconRight="wheat"
+        onClick={() => console.log("click")}
+      />
+      <Button
+        label="Prueba"
+        iconLeft="wheat"
+        iconRight="wheat"
+        variant="secondary"
+        onClick={() => console.log("click")}
+      />
+      <Button
+        label="Prueba"
+        iconLeft="wheat"
+        iconRight="wheat"
+        variant="tertiary"
+        onClick={() => console.log("click")}
+      />
+      <Button
+        disruptive
+        label="Prueba"
+        iconLeft="wheat"
+        iconRight="wheat"
+        size="small"
+        onClick={() => console.log("click")}
+      />
+      <Button
+        disruptive
+        label="Prueba"
+        iconLeft="wheat"
+        iconRight="wheat"
+        variant="secondary"
+        size="medium"
+        onClick={() => console.log("click")}
+      />
+      <Button
+        disruptive
+        label="Prueba"
+        iconLeft="wheat"
+        iconRight="wheat"
+        variant="tertiary"
+        size="large"
+        onClick={() => console.log("click")}
+      />
       <p>esto es un HOME</p>
       <TileGrid previewData={recipePreviews} />
     </div>
